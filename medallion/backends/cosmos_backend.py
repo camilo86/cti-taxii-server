40d5014674e4d85eb041ce33f72dc69550cdea57
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2024-02-13 00:40:17 -0800

    uploads collections, objects & statuses and adds collection queries

--- medallion/backends/cosmos_backend.py
+++ medallion/backends/cosmos_backend.py
@@ -6,11 +6,40 @@ import environ
 from azure.cosmos import CosmosClient, PartitionKey
 from six import string_types
 
+from ..common import (
+    APPLICATION_INSTANCE,
+    create_resource,
+    datetime_to_float,
+    datetime_to_string,
+    datetime_to_string_stix,
+    determine_spec_version,
+    determine_version,
+    float_to_datetime,
+    generate_status,
+    generate_status_details,
+    get_application_instance_config_values,
+    get_custom_headers,
+    get_timestamp,
+    parse_request_parameters,
+    string_to_datetime,
+)
 from ..exceptions import InitializationError
 from .base import Backend
 
 # Module-level logger
 log = logging.getLogger(__name__)
+
+
+def find_manifest_entries_for_id(obj, manifest):
+    for m in manifest:
+        if m["id"] == obj["id"]:
+            if "modified" in obj:
+                if m["version"] == obj["modified"]:
+                    return m
+            else:
+                # handle data markings
+                if m["version"] == obj["created"]:
+                    return m
 
 
 class CosmosBackend(Backend):
@@ -70,10 +99,29 @@ class CosmosBackend(Backend):
             return info[0]
 
     def get_collections(self, api_root):
-        raise NotImplementedError()
+        if api_root not in self.list_databases():
+            return None
+
+        api_root_db = self.client.get_database_client(api_root)
+        collection_info = api_root_db.get_container_client("collections")
+        collections = list(collection_info.read_all_items())
+
+        if get_application_instance_config_values(
+            APPLICATION_INSTANCE, "taxii", "interop_requirements"
+        ):
+            collections = sorted(collections, key=lambda x: x["id"])
+
+        return create_resource("collections", collections)
 
     def get_collection(self, api_root, collection_id):
-        raise NotImplementedError()
+        if api_root not in self.list_databases():
+            return None
+
+        api_root_db = self.client.get_database_client(api_root)
+        collection_info = api_root_db.get_container_client("collections")
+        info = collection_info.read_item(collection_id, collection_id)
+
+        return info
 
     def get_object_manifest(
         self, api_root, collection_id, filter_args, allowed_filters, limit
@@ -133,7 +181,18 @@ class CosmosBackend(Backend):
                 )
 
     def get_api_root_information(self, api_root):
-        raise NotImplementedError()
+        db = self.client.get_database_client("discovery_database")
+        api_root_info = db.get_container_client("api_root_info")
+        info = list(
+            api_root_info.query_items(
+                query="SELECT * FROM c WHERE c._name=@api_root",
+                parameters=[{"name": "@api_root", "value": api_root}],
+                enable_cross_partition_query=True,
+            )
+        )
+
+        if info and len(info) > 0:
+            return info[0]
 
     def get_objects(
         self, api_root, collection_id, filter_args, allowed_filters, limit
@@ -202,6 +261,76 @@ class CosmosBackend(Backend):
             db.get_container_client("discovery_information").upsert_item(
                 self.json_data["/discovery"],
             )
+        else:
+            raise InitializationError(
+                "No discovery information provided when initializing the Mongo"
+            )
+
+        api_root_info_db = db.get_container_client("api_root_info")
+
+        for api_root_name, api_root_data in self.json_data.items():
+            if api_root_name == "/discovery":
+                continue
+
+            url = list(
+                filter(
+                    lambda a: api_root_name in a,
+                    self.json_data["/discovery"]["api_roots"],
+                )
+            )[0]
+            api_root_data["information"]["_url"] = url
+            api_root_data["information"]["_name"] = api_root_name
+
+            api_root_info_db.upsert_item(api_root_data["information"])
+
+            api_db = self.client.create_database(api_root_name)
+            status_container = api_db.create_container(
+                "status", partition_key=PartitionKey(path="/id")
+            )
+
+            if api_root_data["status"]:
+                for status in api_root_data["status"]:
+                    status_container.upsert_item(status)
+
+            collection_container = api_db.create_container(
+                "collections", partition_key=PartitionKey(path="/id")
+            )
+            object_container = api_db.create_container(
+                "objects", partition_key=PartitionKey(path="/id")
+            )
+
+            for collection in api_root_data["collections"]:
+                collection_id = collection["id"]
+                objects = collection["objects"]
+                manifest = collection["manifest"]
+
+                collection.pop("objects")
+                collection.pop("manifest")
+
+                collection_container.upsert_item(collection)
+
+                for obj in objects:
+                    obj["_collection_id"] = collection_id
+                    obj["_manifest"] = find_manifest_entries_for_id(
+                        obj, manifest
+                    )
+                    obj["_manifest"]["date_added"] = datetime_to_float(
+                        string_to_datetime(obj["_manifest"]["date_added"])
+                    )
+                    obj["_manifest"]["version"] = datetime_to_float(
+                        string_to_datetime(obj["_manifest"]["version"])
+                    )
+                    obj["created"] = datetime_to_float(
+                        string_to_datetime(obj["created"])
+                    )
+
+                    if "modified" in obj:
+                        # not for data markings
+                        obj["modified"] = datetime_to_float(
+                            string_to_datetime(obj["modified"])
+                        )
+
+                    object_container.upsert_item(obj)
 
     def list_databases(self):
         return [database["id"] for database in self.client.list_databases()]
